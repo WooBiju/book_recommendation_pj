71081fd0e3bcad0e982208218361351409e680ea
WooBiju <yunji0409@naver.com>
2025-04-20 13:34:22 +0900

    chore: 마이페이지 리뷰 응답 DTO에 별점 소수점 한 자리 반올림 적용

--- src/main/java/com/github/bookproject/mypage/dto/ReviewSummaryDTO.java
+++ src/main/java/com/github/bookproject/mypage/dto/ReviewSummaryDTO.java
@@ -1,10 +1,13 @@
 package com.github.bookproject.mypage.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -26,5 +29,12 @@ public class ReviewSummaryDTO {
         private String content;
         private double rating;
         private LocalDate createdAt;
+
+        // 소수점 한 자리로 반올림된 값 반환
+        public double getRating() {
+            return BigDecimal.valueOf(rating)
+                    .setScale(1, RoundingMode.HALF_UP)
+                    .doubleValue();
+        }
     }
 }
